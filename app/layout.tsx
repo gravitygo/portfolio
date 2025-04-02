37b3aef5a9de69e674e5d6ecd35ca5e8b0ba69ae
gravityo <chyle.andrei.lee@gmail.com>
2025-04-02 16:51:04 +0800

    removed unnecessary props for theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,12 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider>
           <div className="flex min-h-screen flex-col">
             <Header />
             <div className="flex-1">{children}</div>
